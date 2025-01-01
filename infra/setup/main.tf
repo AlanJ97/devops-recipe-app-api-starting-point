1e2a7d89ba5bdc440890fb5f321dc7d135d1f03f
Alan <alannavaseg_02@hotmail.com>
2025-01-01 09:45:51 -0600

    Fixing things according to original teacher code 2

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "5.23.0"
     }
   }
-
+ 
   backend "s3" {
     bucket         = "devops-recipe-app-tf-state-1241"
     key            = "tf-state-setup"
@@ -14,8 +14,10 @@ terraform {
     dynamodb_table = "devops-recipe-app-api-tf-lock"
   }
 }
+  
 provider "aws" {
   region = "us-east-1"
+  
   default_tags {
     tags = {
       Environment = terraform.workspace
