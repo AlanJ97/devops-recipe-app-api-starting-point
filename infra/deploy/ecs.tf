f67f2bc0772553dc02de97ab8340e128d2fff290
Alan <alannavaseg_02@hotmail.com>
2025-01-02 08:57:55 -0600

    added cloudwatch log group

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -36,6 +36,12 @@ resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
   policy_arn = aws_iam_policy.task_ssm_policy.arn
 
 }
+
+resource "aws_cloudwatch_log_group" "ecs_task_logs" {
+  name = "${local.prefix}-api"
+
+}
+
 resource "aws_ecs_cluster" "name" {
   name = "${local.prefix}-cluster"
 }
