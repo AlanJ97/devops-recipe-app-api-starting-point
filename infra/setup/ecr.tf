2175a8af9d60657080ef136a9aafa0e6cb58ab26
Alan <alannavaseg_02@hotmail.com>
2025-01-02 20:14:15 -0600

    changing small things

--- infra/setup/ecr.tf
+++ infra/setup/ecr.tf
@@ -3,7 +3,7 @@
 ##############################################
 
 resource "aws_ecr_repository" "app" {
-  name                 = "recipe-app-api_app"
+  name                 = "recipe-app-api-app"
   image_tag_mutability = "MUTABLE"
   force_delete         = true
 
@@ -14,7 +14,7 @@ resource "aws_ecr_repository" "app" {
 }
 
 resource "aws_ecr_repository" "proxy" {
-  name                 = "recipe-app-api_proxy"
+  name                 = "recipe-app-api-proxy"
   image_tag_mutability = "MUTABLE"
   force_delete         = true
 
